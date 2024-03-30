5f6d005007a89613beca76ba255411dc6c9a0d15
Samuvel L <samdeveloper7550@gmail.com>
2024-03-30 20:47:44 +0530

    Add @vercel/analytics package and integrate Analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
