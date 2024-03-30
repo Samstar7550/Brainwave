1983b8809d0f73d496ed3b96196ac4fa5f3fd5d6
Samuvel L <samdeveloper7550@gmail.com>
2024-03-30 14:52:02 +0530

    first commit

---
+++ src/assets/benefits/index.js
@@ -0,0 +1,25 @@
+import card_1 from "./card-1.svg";
+import card_2 from "./card-2.svg";
+import card_3 from "./card-3.svg";
+import card_4 from "./card-4.svg";
+import card_5 from "./card-5.svg";
+import card_6 from "./card-6.svg";
+import icon_1 from "./icon-1.svg";
+import icon_2 from "./icon-2.svg";
+import icon_3 from "./icon-3.svg";
+import icon_4 from "./icon-4.svg";
+import image_2 from "./image-2.png";
+
+export {
+  card_1,
+  card_2,
+  card_3,
+  card_4,
+  card_5,
+  card_6,
+  icon_1,
+  icon_2,
+  icon_3,
+  icon_4,
+  image_2,
+};
